d1eb3175b3daef5a2532908fd5ee2e91fc44185c
alumno <alumno@Instituto.local>
2025-07-30 08:25:04 -0300

    sgsg

---
+++ objetos.js
@@ -0,0 +1,54 @@
+/*
+let estudiantes = {
+    //clave: valor
+    nombre: 'Bauti',
+    DNI: 49392698,
+    direccion: 'calle 237',
+    fecheNacimiento: '25 de marzo del 2009'
+}
+*/
+
+let estudiantes = [
+    nombre: 'Jazmin ',
+    apellido: 'Rodriguez',
+    edad: 16
+},
+
+{
+    nombre: 'Antoella',
+    apellido: 'Lattandi',
+    edad: 16
+
+},
+ {
+    nombre: 'Matias',
+    apellido: 'Rosello',
+    edad: 16
+ }
+]
+
+for (let indice = 0; indice < estudiantes.length; indice++){
+//mostrar todos los elementos del arreglo
+console.log(estudiantes[indice].apellido + ''+ estudiantes[indice].nombre)
+}
+
+let jugadoresCity = [ {
+    nombre: 'Federico',
+    apellido: 'Laurnagaray',
+    numero: 14,
+    nacionalidad:'N/A'
+},
+{
+nombre: 'Agustin',
+apellido: 'Kramer',
+numero: 14,
+nacionalidad:'N/A'
+}, 
+{
+nombre: 'Antonio',
+apellido: 'Hernandez',
+numero: 14,
+nacionalidad:'N/A'
+}
+]
+console.log(jugador.nombre + '' +jugador.apellido)
