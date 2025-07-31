f6a3aacfced4d6bff0794d0e5ab860811714b722
alumno <alumno@Instituto.local>
2025-07-31 11:24:31 -0300

    fdhgdfhg

--- objetos.js
+++ objetos.js
@@ -8,29 +8,6 @@ let estudiantes = {
 }
 */
 
-let estudiantes = [
-    nombre: 'Jazmin ',
-    apellido: 'Rodriguez',
-    edad: 16
-},
-
-{
-    nombre: 'Antoella',
-    apellido: 'Lattandi',
-    edad: 16
-
-},
- {
-    nombre: 'Matias',
-    apellido: 'Rosello',
-    edad: 16
- }
-]
-
-for (let indice = 0; indice < estudiantes.length; indice++){
-//mostrar todos los elementos del arreglo
-console.log(estudiantes[indice].apellido + ''+ estudiantes[indice].nombre)
-}
 
 let jugadoresCity = [ {
     nombre: 'Federico',
@@ -52,3 +29,55 @@ nacionalidad:'N/A'
 }
 ]
 console.log(jugador.nombre + '' +jugador.apellido)
+
+let estudiante = [ {
+    nombre: 'Maximo',
+    apellido: 'Uriguen',
+    colordePelo: 'rojo',
+    notaExamen: 10
+},{
+    nombre: 'Maximo',
+    apellido: 'Uriguen',
+    colordePelo: 'rojo',
+    notaExamen: 3
+}
+] 
+
+console.log(estudiantesIVT[1].nombre + ' se saco un ' + estudiantesIVT[1].notaExamen)
+
+
+/*nombre apellido edad y nota de examen
+Arreglo de objetos*/
+ let estudiantes = [ {
+     nombre: 'Antonella',
+     apellido: 'Lattandi',
+     edad: 16,
+     notaExamen: 8.50
+ }, 
+ {
+    nombre: 'Damaris',
+    apellido: 'Vilagas',
+    edad: 16,
+    notaExamen: 9
+},  {
+    nombre: 'Matias',
+    apellido: 'Rosello',
+    edad: 16,
+    notaExamen: 8
+},  {
+    nombre: 'Jeronimo',
+    apellido: 'Orfila',
+    edad: 16,
+    notaExamen: 5
+},  {
+    nombre: 'Agustin',
+    apellido: 'Avila',
+    edad: 16,
+    notaExamen: 10
+}
+ ]
+
+ for (let indice = 0; indice < estudiantes.length; indice++) {
+     //mostrar todos los elementos del arreglo
+     console.log(estudinates[indice].apellido + ' ' + estudiantes[indice].nombre)
+ }
